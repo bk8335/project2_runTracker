6ed95dbc5356840170d3e3d7cdc7cd502cce2b31
Benjamin <benjamin.kinnard1@gmail.com>
2017-04-24 17:17:38 +0100

    can change detail of run (but as a new entry, original is left alone)

--- app/src/main/java/example/codeclan/com/runtracker/EditRunDetailsActivity.java
+++ app/src/main/java/example/codeclan/com/runtracker/EditRunDetailsActivity.java
@@ -1,9 +1,14 @@
 package example.codeclan.com.runtracker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 import static example.codeclan.com.runtracker.R.id.pace;
 import static example.codeclan.com.runtracker.R.id.question_input_notes;
@@ -40,6 +45,38 @@ public class EditRunDetailsActivity extends AppCompatActivity {
 
         String runningNotes = bundle.getString("run_notes");
         runNotes.setText(runningNotes);
+
+    }
+
+
+    public void onUpdateRunButtonClicked(View button) {
+//       this is where i use the sharedpreferences to setRunList
+//       onCreate, cast. onClick, turn your cast into a string i.e question_input_date.getText().toString()
+//        if not expecting a string, need to do parseint or similar to put it in the required format.
+
+        String dateAnswer = runDateText.getText().toString();
+//        SimpleDateFormat datedateAnswer = new SimpleDateFormat("dd-mmm-yyyy");
+//        java.util.Date stringDate = datedateAnswer.parse(dateAnswer);
+
+        String distanceAnswer = runDistanceText.getText().toString();
+        double doubleDistanceAnswer = Double.parseDouble(distanceAnswer);
+
+        String timeAnswer = runTimeMinutesText.getText().toString();
+        int intTimeAnswer = Integer.parseInt(timeAnswer);
+
+        String noteAnswer = runNotes.getText().toString();
+
+        Log.d(getClass().toString(), "on update run button clicked");
+
+
+        ArrayList<Run> runList = SharedPreferencesManager.getRunList(this);
+        runList.add(new Run(dateAnswer, doubleDistanceAnswer, intTimeAnswer, noteAnswer));
+
+        SharedPreferencesManager.setRunList(this, runList);
+
+
+        Intent intent = new Intent(this, RunListViewActivity.class);
+        startActivity(intent);
 
     }
 }
