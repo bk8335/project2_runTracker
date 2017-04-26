65383d2c707345b250e47b49962603e0bc69d494
Benjamin <benjamin.kinnard1@gmail.com>
2017-04-26 11:13:00 +0100

    hardcoded values in total running page

--- app/src/main/java/example/codeclan/com/runtracker/AllRunsActivity.java
+++ app/src/main/java/example/codeclan/com/runtracker/AllRunsActivity.java
@@ -6,10 +6,15 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class AllRunsActivity extends AppCompatActivity {
+
+    TextView totalRunDistanceText;
+    TextView totalRunTimeText;
+    TextView totalRunPaceText;
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -37,15 +42,41 @@ public class AllRunsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_runs);
+
+        totalRunDistanceText = (TextView)findViewById(R.id.total_distance);
+        totalRunTimeText = (TextView)findViewById(R.id.total_time);
+        totalRunPaceText = (TextView)findViewById(R.id.total_pace);
+
+        totalRunDistanceText.setText(totalRunDistance().toString());
+        totalRunTimeText.setText(totalRunTime().toString());
+        totalRunPaceText.setText(totalRunPace());
+
+
     }
 
-    public double totalRunDistance() {
-        double runDistance = 0;
+    public Double totalRunDistance() {
+        double runDistance = 10;
         ArrayList<Run> runList = SharedPreferencesManager.getRunList(this);
         for(Run run : runList){
 //            run.getDistance() += runDistance;
         }
 
         return runDistance;
+    }
+
+    public Integer totalRunTime() {
+        int runTime = 45;
+        return runTime;
+    }
+
+    public String totalRunPace() {
+        //        ((get precise number - rounded down number) * 60) + rounded down number
+        Double mins = Math.floor(totalRunTime() / totalRunDistance());
+        Double secs = Math.floor(((totalRunTime()/totalRunDistance()) - Math.floor(totalRunTime() / totalRunDistance())) * 60);
+
+        String minsRounded = String.format("%.0f", mins);
+        String secsRounded = String.format("%.0f", secs);
+
+        return minsRounded + ":" + secsRounded;
     }
 }
